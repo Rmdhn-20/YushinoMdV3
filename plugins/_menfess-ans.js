2c5f867fb143b9a065a8c412dabdc0983f3e2892
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-11-06 00:25:34 +0700

    test

--- plugins/_menfess-ans.js
+++ plugins/_menfess-ans.js
@@ -1,42 +1,18 @@
-import fs from 'fs'
-const delay = time => new Promise(res => setTimeout(res, time))
-export async function before(m) {
-	if (!m.chat.endsWith('@s.whatsapp.net')) return !0;
-	this.menfess = this.menfess ? this.menfess : {}
-	let mf = Object.values(this.menfess).find(v => v.status === false && v.penerima == m.sender)
-	if (!mf) return !0
-	console.log({ text: m.text, type: m.quoted?.mtype })
-	if ((m.text === 'BALAS PESAN' || m.text === '') && m.quoted.mtype == 'buttonsMessage') return m.reply("Silahkan kirim pesan balasan kamu.\nKetik pesan sesuatu lalu kirim, maka pesan otomatis masuk ke target balas pesan.");
-	else {
-		let imgr = fla.getRandom()
-		let txt = `Hᴀɪ ᴋᴀᴋ @${mf.dari.split('@')[0]}, ᴋᴀᴍᴜ ᴍᴇɴᴇʀɪᴍᴀʜ ʙᴀʟᴇsᴀɴ ɴɪʜ.Pesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
-		let sblm = `Pᴇsᴀɴ Aɴᴅᴀ Sᴇʙᴇʟᴜᴍɴʏᴀ ➛ ${mf.pesan}\nPᴇsᴀɴ Bᴀʟᴀsᴀɴɴʏᴀ ➨ ${m.text}`
-		await this.send3ButtonDoc(mf.dari, txt, sblm, 'Balas', '.balasmenfess', 'Owner', '.owner', 'Credit', '.credit', fakes, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://instagram.com/_b4c00t4an_s3l3b',
-    mediaType: 2, 
-    description: sgc,
-    title: "Jᴏɪɴ Sɪɴɪ Cᴜʏ",
-    body: wm,
-    thumbnail: fs.readFileSync('./media/menfess.jpg'),
-    sourceUrl: sgc}}})
-		/*await this.sendButton(mf.dari, bottime, txt, `${imgr + 'Menfess'}`, [['BALAS PESAN', '.balasmenfess']], null)*/.then(() => {
-			m.reply('Berhasil Mengirim balasan.')
-			delay(1500)
-			delete this.menfess[mf.id]
-			return !0
-		})
-	}
-	return !0
-}
-/* Made By FokusDotId (Fokus ID)
- * https://github.com/FokusDotId
- * Ingin bikin fitur tapi tidak bisa coding?
- * hubungi: https://wa.me/6281320170984
- * Jangan lupa bawa udut minimal sukun🗿
- 
- *Nᴜᴍᴘᴀɴɢ Nᴀᴍᴀ*
- *Kᴀɴɢ Rᴇᴄᴏᴅᴇ : Aʟᴅɪ Lᴇsᴍᴀɴᴀ
- *Cʜᴀɴɴᴇʟ : AL? Offc
- 
- *Minimal Jangan Di Hapos Anj
-*/
+export async function before(m, { match }) {
+    try {
+      conn.menfess = conn.menfess ? conn.menfess : {}
+      const find = Object.values(conn.menfess).find(menpes => [menpes.a, menpes.b].includes(m.sender) && menpes.status == 'chatting')
+          if(m.isGroup) return
+          if(find == undefined) return
+          const to = find.a == m.sender ? find.b : find.a
+          m.copyNForward(to, true)
+    } catch {}
+  }
+  
+
+// By Ekuzika
+//cuma fix dikit
+//Thx To
+//xzeera-id
+//ayang gw:v (citlaa_12)
+
--- plugins/main-menfess.js
+++ plugins/main-menfess.js
@@ -1,72 +1,82 @@
 import fs from 'fs'
-let handler = async (m, { conn, number, text, usedPrefix, command }) => {
-let tag = `@${m.sender.replace(/@.+/, '')}`
-let mentionedJid = [m.sender] 
-  
+const delay = time => new Promise(res => setTimeout(res, time))
+
+async function handler(m, { command }) {
+
     conn.menfess = conn.menfess ? conn.menfess : {}
-    if (!text) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
+    let id = m.sender
+    find = Object.values(conn.menfess).find(menpes => menpes.status == 'wait')
+    roof = Object.values(conn.menfess).find(menpes => [menpes.a, menpes.b].includes(m.sender))
+
+    switch(command) {
+    case 'menfes': case 'menfess':
+    if(roof) return m.reply('Kamu masih berada dalam Obrolan!')
     let [jid, name, pesan] = text.split('|');
     if ((!jid || !name || !pesan)) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
     jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
     let data = (await conn.onWhatsApp(jid))[0] || {};
-    if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.';
-    if (jid == m.sender) throw 'tidak bisa mengirim pesan menfess ke diri sendiri.'
-    let mf = Object.values(conn.menfess).find(mf => mf.status === true)
-    if (mf) return !0
- 
-let id = + new Date
-let tek = `Hᴀɪ Kᴀᴋ @${data.jid.split('@')[0]}, ᴋᴀᴍᴜ ᴍᴇɴᴇʀɪᴍᴀʜ ᴘᴇsᴀɴ ᴍᴀɴғᴇss ɴɪʜ.\n➴`.trim();
-let logs = `➯ Pᴇsᴀɴ : ${pesan}`
+    if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.'
+    if (jid == m.sender) throw 'Kenapa kirim menfess ke diri sendiri kak?\nCapek ya? Semangat kak<3'
 
-let ssn = `〠 Dᴀʀɪ : ${name}\n⎙ Pᴇsᴀɴ : ${pesan}`
-        let imgr = fla.getRandom()
-        //Created By AL? Offc
-       await conn.send3ButtonDoc(data.jid, tek, ssn, 'Balas', '.balasmenfess', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://instagram.com/_b4c00t4an_s3l3b',
+    let tek = `Hᴀɪ Kᴀᴋ @${data.jid.split('@')[0]}, ᴋᴀᴍᴜ ᴍᴇɴᴇʀɪᴍᴀʜ ᴘᴇsᴀɴ ᴍᴀɴғᴇss ɴɪʜ.\n➴`.trim();
+    let ssn = `〠 Dᴀʀɪ : ${name}\n⎙ Pᴇsᴀɴ : ${pesan}\nJam : ${wib}`
+    let logs = `➯ Pᴇsᴀɴ : ${pesan}\nJam : ${wib}`
+    let imgr = fla.getRandom()
+
+    await conn.send2ButtonDoc(data.jid, tek, ssn, 'Lanjutkan', '.accmenfess', 'Tolak Menfess', '.tolakmenfess', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: sig,
     mediaType: 2, 
     description: sgc,
-    title: "Jᴏɪɴ Sɪɴɪ Cᴜʏ",
+    title: "Follow Sɪɴɪ Cᴜʏ",
     body: wm,
     thumbnail: fs.readFileSync('./media/menfess.jpg'),
-    sourceUrl: sgc
-   }}}).then(() => {
-           conn.send3ButtonDoc(m.chat, `Bᴇʀʜᴀsɪʟ Mᴇɴɢɪʀɪᴍ Pᴇsᴀɴ Kᴇ @${jid.replace(/@.+/, '')}`, logs, 'Menu', '.menu', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://facebook.com/sadtime098',
-    mediaType: 2, 
-    description: sgc,
-    title: "Jᴏɪɴ Sɪɴɪ Cᴜʏ",
-    body: wm,
-    thumbnail: fs.readFileSync('./media/menfess.jpg'),
-    sourceUrl: sgc
-   }}})
-            conn.menfess[id] = {
-                id,
-                dari: m.sender,
-                nama: name,
-                penerima: data.jid,
-                pesan: pesan,
-                status: false
-            }
-            return !0
-        })
+    sourceUrl: sig
+    }}})
+    conn.sendButton(m.chat, `*Done* mengirim menfess kepada @${data.jid.split('@')[0]}`, logs, imgr, [['Menu', '.? all']], m)
+    conn.menfess[id] = {
+          id: id,
+          a: m.sender,
+          b: jid,
+          status: "wait",
+        }
+        break;
+
+    case 'accmenfess':
+        if(!roof) return m.reply("Kamu belum memulai menfess..")
+        try {
+            find.b = m.sender
+            find.status = 'chatting'
+            conn.menfess[find.id] = {...find}
+            find = Object.values(conn.menfess).find(menpes => [menpes.a, menpes.b].includes(m.sender))
+            conn.sendMessage(find.a, `_@${m.sender.split("@")[0]} menerima chat dengan anda, sekarang anda bisa melanjutkan menfes dengan dia.._\n\n*NOTE : Jika ingin berhenti dari menfess, silahkan ketik _.stopmenfess_ Untuk hapus session kalian..*`, m)
+            m.reply(`*^Done..*\n\nSekarang kamu bisa melanjutkan menfes dengan dia. Ketik sesuatu, bot akan otomatis mengirim pesan mu.\n\n*NOTE : Jika ingin berhenti dari menfess, silahkan ketik _.stopmenfess_ Untuk hapus session kalian..*`)
+        } catch (e){
+            m.reply(e)
+        }
+            break
     
+    case 'tolakmenfess':
+        if(!roof) return m.reply("Kamu belum memulai menfess..")
+        find = Object.values(conn.menfess).find(menpes => [menpes.a, menpes.b].includes(m.sender))
+        await conn.sendMessage(find.a, `_@${find.b.split("@")[0]} Menolak bermain menfess.._`, m)
+        m.reply("*^Done..*")
+        delete conn.menfess[find.id]
+        return !0
+        break;
     }
-handler.tags = ['main']
+}
+
+handler.tags = ['anonymous']
 handler.help = ['menfess'].map(v => v + ' <nomor|nama|pesan>')
-handler.command = /^(menfess|menfes)$/i
+handler.command = ['menfess', 'menfes', 'accmenfess', 'tolakmenfess']
+
 handler.private = true
 
 export default handler
 
-/* Made By FokusDotId (Fokus ID)
- * https://github.com/FokusDotId
- * Ingin bikin fitur tapi tidak bisa coding?
- * hubungi: https://wa.me/6281320170984
- * Jangan lupa bawa udut minimal sukun🗿
- 
- *Nᴜᴍᴘᴀɴɢ Nᴀᴍᴀ
- *Kᴀɴɢ Rᴇᴄᴏᴅᴇ : Aʟᴅɪ Lᴇsᴍᴀɴᴀ
- *Cʜᴀɴɴᴇʟ : AL? Offc
- 
- *Minimal Jangan Di Hapos Anj
-*/
+// By Ekuzika
+//cuma fix dikit
+//Thx To
+//xzeera-id
+//ayang gw:v (citlaa_12)
+
