72d796cb2ee7d880b129cd0e52d50266d1607ccf
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-11-08 09:43:36 +0700

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, isROwner }) => {
 }
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = /^update$/i
+handler.command = /^(update)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
