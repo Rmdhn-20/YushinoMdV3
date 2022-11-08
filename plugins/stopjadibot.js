2833ad73de558d4837dcb00abf6e3e6894f2f71e
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-11-08 11:01:02 +0700

    Update stopjadibot.js

--- plugins/stopjadibot.js
+++ plugins/stopjadibot.js
@@ -10,18 +10,8 @@ let handler  = async (m, { conn, text}) => {
 handler.help = ['berhenti','stop']
 handler.tags = ['premium']
 handler.command = /^(berhenti|stop|stopjadibot|berhentijadibot)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
-module.exports = handler
+export default handler
 
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
